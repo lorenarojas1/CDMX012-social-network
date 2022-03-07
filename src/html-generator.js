971558c687f76cf242a5ab825d52f2c365875e41
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-03-07 10:03:39 -0600

    registro e inicio de sesión

--- src/html-generator.js
+++ src/html-generator.js
@@ -1,38 +1,39 @@
 export function logInHtml() {
-    return `
+  return /* html */ `
+
+   
     <section class="logInEmail">
-                <input type="email" class="inputEmail" id="inputEmail" placeholder="Correo Electrónico" </div>
-                <input type="password" class="passwordEmail" id="passwordEmail" placeholder="Contraseña">
-                <div class="helpPassword">
-                    <label><input type="checkbox" class="checkboxRememberPass" id="checkboxRememberPass" value="rememberPassword">Guardar contraseña</label>
-                    <p class="questionPassword">¿Olvidaste tu contraseña?</p>
-                </div>
-                <button type="button" class="logIn" id="buttonLogIn">Iniciar sesión</button>
-                <p class="questionLogIn">¿No tienes una cuenta?</p>
-                <p>Registrate</p>
-                <div class="linea"></div>
-                <p class="logInWith">O ingresa con</p>
-            </section>
+      <input type="email" class="inputEmail" id="inputEmail" placeholder="Correo Electrónico">
+      <input type="password" class="inputPasswordEmail" id="passwordEmail" placeholder="Contraseña">
+      <p class="questionPassword">¿Olvidaste tu contraseña?</p>
+      <button type="button" class="logIn" id="buttonLogIn">Iniciar sesión</button>
+      <p class="questionLogIn">¿No tienes una cuenta?  <a href="#">Registrate</a></p>
+      <div class="linea"></div>
+      <p class="logInWith">O ingresa con</p>
+    </section>
     `;
-};
+}
 
 export function singInHtml() {
-    return `
-    <section class="singInEmail">
-        <input type="email" class="inputEmail" id="inputEmail" placeholder="Correo Electrónico" </div>
-        <input type="password" class="passwordEmail" id="passwordEmail" placeholder="Contraseña">
-        <input type="password" class="confirmPasswordEmail" id="confirmPasswordEmail" placeholder="Confirmar Contraseña">
-          <div class="privacyTerms">
-          <label><input type="checkbox" class="checkboxPrivacyTerms" id="checkboxPrivacyTerms" value="acceptPrivacyTerms">Estoy de acuerdo con los términos de privacidada</label>
-          </div>
-        <button type="button" class="singIn" id="buttonSingIn">Registrarse</button>
-          <p class="questionSingIn">¿Tienes una cuenta?</p>
-          <p>Inicia sesión</p>
-        <div class="linea"></div>
-          <p class="signInWith">O registrate con</p>
+  return /* html */ `
+
+    
+      <section class="singInEmail">
+      <input type="email" class="inputEmail" id="inputEmail" placeholder="Correo Electrónico">
+      <input type="password" class="inputPasswordEmail" id="passwordEmail" placeholder="Contraseña">
+      <input type="password" class="inputConfirmPasswordEmail" id="confirmPasswordEmail" placeholder="Confirmar Contraseña">
+      <div class="privacyTerms">
+        <label>
+          <input type="checkbox" class="checkboxPrivacyTerms" id="checkboxPrivacyTerms" value="acceptPrivacyTerms">
+          Estoy de acuerdo con los términos de privacidada
+        </label>
+      </div>
+      <button type="button" class="singIn" id="buttonSingIn">Registrarse</button>
+      <p class="questionSingIn">¿Tienes una cuenta?</p>
+      <p>Inicia sesión</p>
+      <div class="linea"></div>
+      <p class="signInWith">O registrate con</p>
     </section>
 
 `;
-};
-
-// window.customElements.define(my - first - CompositionEvent, MyFirstComponent);
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { logInHtml, singInHtml } from './html-generator.js';
+import { signIn, logIn, userState } from './firebase.js';
 
 const btnLogInEmail = document.getElementById("btnLogIn");
 const fillHtml = document.querySelector("#welcomeContent");
@@ -6,74 +7,70 @@ const btnSingInEmail = document.getElementById("btnSingIn");
 const regExEmail = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
 
 
-
-btnLogInEmail.addEventListener("click", () => {
-    fillHtml.innerHTML = logInHtml();
-
-    const buttonLogInEmail = document.getElementById("buttonLogIn");
-    const email = document.getElementById("inputEmail");
-    const password = document.getElementById("passwordEmail");
-
-    buttonLogInEmail.addEventListener("click", () => {
-
-        if ((email.value && password.value) === "") {
-            return console.log("llene todos los campos");
-
-        } else if ((regExEmail.test(email.value)) != true) {
-            return console.log("ingrese un correo válido");
-        }
-        document.getElementById("inputEmail").value = "";
-        document.getElementById("passwordEmail").value = "";
-    });
-
-});
+console.log(userState());
 
 btnSingInEmail.addEventListener("click", () => {
-    fillHtml.innerHTML = singInHtml();
+  fillHtml.innerHTML = singInHtml();
 
-    const buttonSingIn = document.getElementById("buttonSingIn");
-    const email = document.getElementById("inputEmail");
-    const password = document.getElementById("passwordEmail");
-    const confirmPasswordEmail = document.getElementById("confirmPasswordEmail");
-    //const regExSpace = /^\s/
+  const buttonSingIn = document.getElementById("buttonSingIn");
+  const email = document.getElementById("inputEmail");
+  const password = document.getElementById("passwordEmail");
+  const confirmPasswordEmail = document.getElementById("confirmPasswordEmail");
+  //const regExSpace = /^\s/
 
-    buttonSingIn.addEventListener("click", () => {
-        //antes poner que los campos no contengan espacios vacios, [a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}
-        if ((email.value && password.value && confirmPasswordEmail.value) === "") {
-            return console.log("llene todos los campos");
+  buttonSingIn.addEventListener("click", async() => {
+    //antes poner que los campos no contengan espacios vacios, [a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}
+    if ((email.value && password.value && confirmPasswordEmail.value) === "") {
+      return console.log("llene todos los campos");
 
-        } else if ((regExEmail.test(email.value)) != true) {
-            return console.log("ingrese un correo válido");
-        }
-        //considerar numero de caracteres minimos o maximos, y cuales no son válidos
-        else if (password.value != confirmPasswordEmail) {
-            return console.log("contraseñas no coinciden");
+    } else if ((regExEmail.test(email.value)) != true) {
+      return console.log("ingrese un correo válido");
+    }
+    //considerar numero de caracteres minimos o maximos, y cuales no son válidos
+    else if (password.value != confirmPasswordEmail.value) {
+      return console.log("contraseñas no coinciden");
 
-        }
-        document.getElementById("inputEmail").value = "";
-        document.getElementById("passwordEmail").value = "";
-        document.getElementById("confirmPasswordEmail").value = "";
+    }
 
-    })
+    const userSignIn = await signIn(email.value, password.value);
+    console.table(userSignIn);
+
+
+    document.getElementById("inputEmail").value = "";
+    document.getElementById("passwordEmail").value = "";
+    document.getElementById("confirmPasswordEmail").value = "";
+
+  });
 
 });
 
+btnLogInEmail.addEventListener("click", () => {
+
+  fillHtml.innerHTML = logInHtml();
+
+  const buttonBack = document.getElementById("back");
+  const buttonLogInEmail = document.getElementById("buttonLogIn");
+  const email = document.getElementById("inputEmail");
+  const password = document.getElementById("passwordEmail");
+
+
+  buttonLogInEmail.addEventListener("click", async() => {
+
+    if ((email.value && password.value) === "") {
+      return console.log("llene todos los campos");
+
+    } else if ((regExEmail.test(email.value)) != true) {
+      return console.log("ingrese un correo válido");
+    }
+
+    const userLogIn = await logIn(email.value, password.value);
+    console.log('jhovanni', userLogIn);
+    console.log('ghfdgxfxdsdfs', userState());
+
+    //document.getElementById("inputEmail").value = "";
+    //document.getElementById("passwordEmail").value = "";
+  });
 
 
 
-
-
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
-// const auth = getAuth();
-// createUserWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//         // Signed in
-//         const user = userCredential.user;
-//         // ...
-//     })
-//     .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         // ..
-//     });
+});

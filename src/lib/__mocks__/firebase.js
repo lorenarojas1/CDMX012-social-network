dbb25262777a08e36180e8b118026e7fb6dd0bc8
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-03-23 21:57:42 -0600

    pruebas unitarias signin.js

---
+++ src/lib/__mocks__/firebase.js
@@ -0,0 +1,41 @@
+// import { signInFirebase } from "../firebase";
+
+/* eslint-disable no-unused-vars */
+const getAuth = jest.fn(() => {
+
+});
+
+const signOut = (auth) => Promise.resolve({});
+
+const createUserWithEmailAndPassword = (auth, email, password) => Promise.resolve({});
+
+const signInWithEmailAndPassword = (auth, email, password) => Promise.resolve({});
+
+const onAuthStateChanged = (auth) => Promise.resolve({});
+
+const sendSignInLinkToEmail = (auth, email, actionCodeSettings) => Promise.resolve({});
+
+const initializeApp = (firebaseConfig) => {
+
+};
+
+const getAnalytics = (app) => {
+
+};
+
+const signInFirebase = (email, password) => Promise.resolve({});
+
+export { getAuth };
+export { signOut };
+export { createUserWithEmailAndPassword };
+export { signInWithEmailAndPassword };
+export { onAuthStateChanged };
+export { sendSignInLinkToEmail };
+export { initializeApp };
+export { getAnalytics };
+
+// export {
+//   getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword,
+//   onAuthStateChanged, sendSignInLinkToEmail, initializeApp, getAnalytics,
+// };
+export { signInFirebase };
---
+++ test/lib/signinFirebase.spec.js
@@ -0,0 +1,12 @@
+import { signInFirebase } from '../../src/lib/firebase.js';
+// import { createUserWithEmailAndPassword } from '../../src/lib/__mocks__/firebase.js';
+
+jest.mock('../../src/lib/firebase.js');
+
+describe('signInFirebase', () => {
+  // eslint-disable-next-line jest/no-focused-tests
+  it('deberia regresar usuario', async () => {
+    const resultado = await signInFirebase('q@q.com', '123456');
+    expect(resultado).toStrictEqual({});
+  });
+});
